2320ddafc17611cf0ea4d56f66973adf243061ec
Fxstar.eu <hello@fxstar.eu>
2017-02-15 12:16:15 +0100

    Update func.php

--- func.php
+++ func.php
@@ -1,3 +1,30 @@
+<?php
+// CURL GET SSL force cert or how add cacert.pem http://stackoverflow.com/questions/4372710/php-curl-https
+function curl($url){
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch,CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	$out = curl_exec($ch);
+	// Check HTTP status code
+	if (!curl_errno($ch)) {
+	  switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+	    case 200:  # OK
+	      break;
+	    default:
+	      return $out = '['.$http_code.']';
+	  }
+	}
+	curl_close($ch);
+	return $out;
+}
+// how to use
+echo curl('https://fxstar.eu');
+?>
+
+<?php
 function parseHeaders( $headers )
 {
     $head = array();
