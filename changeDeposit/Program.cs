7b3db2ce6be82a8155dedfdd554f8bfae7a36154
Fxstar.eu <hello@fxstar.eu>
2017-03-12 16:03:10 +0100

    Update Program.cs

--- changeDeposit/Program.cs
+++ changeDeposit/Program.cs
@@ -55,7 +55,11 @@ namespace DepositZero
             // change account deposit  
             string o = Deposit(apiUrl,accessToken,accountId,amount);
             Console.WriteLine(o);
-
+            
+            // Change account deposit to initialDeposit for accounts connected to accessToken
+            string acc = ChangeAccountsDeposit(accessToken);
+            Console.WriteLine(acc);
+            
             // Send POST data with web client
             WebClient client = new WebClient();
             string url = "http://fo.x/send.php";
@@ -87,5 +91,60 @@ namespace DepositZero
             request.AddBody(new { amount = amount });
             return client.Execute(request).StatusDescription;
         }
+        
+        // Change all accounts depositi to initialDeposit
+        public static string ChangeAccountsDeposit(string accessToken, double initialDeposit = 1000000)
+        {
+            string urlAccounts = "https://api.spotware.com/connect/tradingaccounts?access_token=" + accessToken;
+            string cAccounts = "";
+            try
+            {
+                var wc = new WebClient();                
+                // Get account from accessToken
+                cAccounts = wc.DownloadString(urlAccounts);
+                dynamic accounts = JsonConvert.DeserializeObject<dynamic>(cAccounts);
+                foreach (var item in accounts.data)
+                {
+                    Console.WriteLine(item.accountId + " " + item.balance);
+                    double balance = Double.Parse((string)item.balance)/100;
+                    Console.WriteLine("Balance " + balance);
+                    if (balance > initialDeposit)
+                    {
+                        double amount = Math.Round(balance - initialDeposit, 2);                        
+                        amount = amount * 100;                        
+                        Withdraw(accessToken, (string)item.accountId, amount.ToString());
+                        Console.WriteLine("Zmiejszam deposit " + amount);
+                    }
+                    if (balance < initialDeposit)
+                    {
+                        double amount = Math.Round(initialDeposit - balance, 2);
+                        amount = amount * 100;                        
+                        Deposit(accessToken, (string)item.accountId, amount.ToString());
+                        Console.WriteLine("Zwiększam deposit " + amount);
+                    }                    
+                }
+            }
+            catch (Exception ee)
+            {
+                Console.WriteLine("Coś nie tak z pobieraniem kont" + ee.ToString());
+            }
+            return cAccounts;
+        }
+
+        public static string GetDeals(string accountId, string accessToken, Int32 HistoryDays = 3)
+        {
+            WebClient pos = new WebClient();
+            Int64 fromTimestamp = (Int64)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+            fromTimestamp = fromTimestamp - (60 * 60 * 24 * HistoryDays);
+            fromTimestamp = fromTimestamp * 1000;
+            try
+            {
+                string Deals = pos.DownloadString("https://api.spotware.com/connect/tradingaccounts/" + accountId + "/deals?access_token=" + accessToken + "&fromTimestamp=" + fromTimestamp);
+                return Deals;
+            }catch (Exception e){
+                Console.WriteLine("Error account");
+                return "0";
+            }            
+        }        
     }
 }
